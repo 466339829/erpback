1feb36e2f67068b443e7049bb546128c07d8a86f
2271789293 <2271789293@qq.com>
2021-06-24 14:33:05 +0800

    ly

--- domain/src/main/java/com/guigu/erp/pojo/PayDetails.java
+++ domain/src/main/java/com/guigu/erp/pojo/PayDetails.java
@@ -6,6 +6,11 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
 @Data
--- service/src/main/java/com/guigu/erp/service/PayService.java
+++ service/src/main/java/com/guigu/erp/service/PayService.java
@@ -1,7 +1,9 @@
 package com.guigu.erp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.pagehelper.PageInfo;
 import com.guigu.erp.pojo.Pay;
 
 public interface PayService extends IService<Pay> {
+    PageInfo<Pay> page(Integer pageNO,Integer pageSize);
 }
